a5bc9f35596a85154fc6a0a0d1e8b147decd58e8
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2022-12-31 10:14:04 -0500

    Browser Activities

---
+++ Introduction/src/WindowActivities.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowActivities {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+
+		driver.get("http://google.com");
+
+		driver.navigate().to("https://rahulshettyacademy.com");
+
+		driver.navigate().back();
+		
+		Thread.sleep(2000);
+
+		driver.navigate().forward();
+		
+		driver.close();
+
+	}
+
+}
