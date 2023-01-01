6c8476bdc78aca7b4cadd740e819f75426a7b59c
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-01-01 10:52:50 -0500

    Checkbox

---
+++ Introduction/src/Checkbox.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Checkbox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		
+		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+		
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		
+		//Count the number of checkboxes
+		
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		
+		driver.close();
+		
+	}
+
+}
