d0758db1c46272d817dffcd8f29ed8b4f75be346
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-01-02 10:46:00 -0500

    UI Assignment 2

--- Introduction/src/Assignment2.java
+++ Introduction/src/Assignment2.java
@@ -19,11 +19,8 @@ public static void main(String[] args) {
 
 // TODO Auto-generated method stub
 
-System.setProperty("webdriver.chrome.driver", "/Users/gilla/Documents/chromedriver");
-
-
-
-WebDriver driver=new ChromeDriver();
+	System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
 
 driver.get("https://rahulshettyacademy.com/angularpractice/");
 
