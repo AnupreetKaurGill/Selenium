bca862ae53c97e7ddd3ea8a04e70d99543c8cbfe
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-01-08 09:13:50 -0500

    Mouse Interactions

--- Introduction/src/ActionsDemo.java
+++ Introduction/src/ActionsDemo.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
@@ -14,8 +16,11 @@ public class ActionsDemo {
 		
 		driver.get("https://www.amazon.com/");
 		
+		WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
+		
 		Actions a = new Actions(driver);
-		a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
+		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").build().perform();
+		a.moveToElement(move).contextClick().build().perform();
 
 		
 	}
