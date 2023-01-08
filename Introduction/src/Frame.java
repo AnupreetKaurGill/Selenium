5c8234874d3861a9567119f8aa0608eb29e84ed1
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-01-08 10:34:20 -0500

    Frames changes

--- Introduction/src/Frame.java
+++ Introduction/src/Frame.java
@@ -14,13 +14,17 @@ public class Frame {
 		driver.get("http://jqueryui.com/droppable/");
 		
 		
-		driver.switchTo().frame(driver.findElement(By.cssSelector("iframe.demo-frame")));
+		//System.out.println(driver.findElements(By.tagName("iframe")).size());
+		//driver.switchTo().frame(0);
 		
+		driver.switchTo().frame(driver.findElement(By.cssSelector("iframe.demo-frame")));
+			
 		Actions a = new Actions(driver);
 		WebElement source = driver.findElement(By.id("draggable"));
 		WebElement target = driver.findElement(By.id("droppable"));
 		
 		a.dragAndDrop(source, target).build().perform();
+		driver.switchTo().defaultContent();
 		//driver.close();
 	}
 
