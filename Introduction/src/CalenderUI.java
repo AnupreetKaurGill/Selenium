ed54ea31c25401c07ba2d35ffeedddfac9e43e2f
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-01-09 09:27:28 -0500

    Calendar UI

---
+++ Introduction/src/CalenderUI.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CalenderUI {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.path2usa.com/travel-companions");
+		
+		//April 23
+		driver.findElement(By.xpath(".//*[@id='travel_date']")).click();
+
+
+		while(!driver.findElement(By.cssSelector("[class='datepicker-days'] [class='datepicker-switch']")).getText().contains("May"))
+		{
+		driver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
+		}
+
+
+		List<WebElement> dates= driver.findElements(By.className("day"));
+		//Grab common attribute//Put into list and iterate
+		int count=driver.findElements(By.className("day")).size();
+
+		for(int i=0;i<count;i++)
+		{
+		String text=driver.findElements(By.className("day")).get(i).getText();
+		if(text.equalsIgnoreCase("21"))
+		{
+		driver.findElements(By.className("day")).get(i).click();
+		break;
+		}
+
+		}
+
+	}
+
+}
