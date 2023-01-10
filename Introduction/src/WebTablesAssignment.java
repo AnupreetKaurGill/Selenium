63c402a90c6badcd24bd6d25a75b82fa9977a176
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-01-10 08:29:34 -0500

    WebTable Assignment

---
+++ Introduction/src/WebTablesAssignment.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebTablesAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://qaclickacademy.com/practice.php");
+		WebElement table=driver.findElement(By.id("product"));
+		System.out.println(table.findElements(By.tagName("tr")).size());
+		
+		System.out.println(table.findElements(By.tagName("tr")).get(0).findElements(By.tagName("th")).size());
+		List<WebElement> secondrow=table.findElements(By.tagName("tr")).get(2).findElements(By.tagName("td"));
+		
+		for(int i =0; i<secondrow.size();i++) {
+			
+			System.out.println(secondrow.get(i).getText());
+		}
+
+	}
+
+}
