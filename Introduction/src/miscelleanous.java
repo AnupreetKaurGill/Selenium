052f0abebfe7d8cbdaa896e35636c1447825bd59
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-01-11 09:03:47 -0500

    delete cookies and screenshot

---
+++ Introduction/src/miscelleanous.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.commons.io.FileUtils;
+
+public class miscelleanous {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+//		driver.manage().deleteCookieNamed("sessionKey");
+		
+		//click on any link
+		//login page- verify login url
+		
+		
+		driver.get("http://google.com");
+		File src=	 ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src,new File("C:\\Users\\gilla\\OneDrive\\Pictures\\Screenshots\\screenshot.png"));
+
+	}
+
+}
