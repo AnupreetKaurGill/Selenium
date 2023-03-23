2b575668a5affad719af76e5ae4cb7822087f312
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-03-23 12:50:33 -0400

    maven project created

---
+++ SeleniumFrameworkDesign/src/test/java/automation/StandAloneTest.java
@@ -0,0 +1,24 @@
+package automation;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StandAloneTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/client");
+
+	}
+
+}
