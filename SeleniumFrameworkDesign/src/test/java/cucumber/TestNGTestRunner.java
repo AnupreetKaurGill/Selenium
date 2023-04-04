0e8489cf59a8b7ec28f7db9e94d60468e6ba5763
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-04-04 13:05:12 -0400

    TestNG Runner

---
+++ SeleniumFrameworkDesign/src/test/java/cucumber/TestNGTestRunner.java
@@ -0,0 +1,12 @@
+package cucumber;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features="src/test/java/cucumber",glue="automation.stepDefinitions",
+monochrome=true, tags = "@Regression", plugin= {"html:target/cucumber.html"})
+
+public class TestNGTestRunner extends AbstractTestNGCucumberTests{
+
+	
+}
