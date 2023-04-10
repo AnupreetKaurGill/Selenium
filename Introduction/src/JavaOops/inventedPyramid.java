f767e84d893f0b6f07c66190878e4b08534dcd91
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-04-10 12:35:11 -0400

    nested loops

---
+++ Introduction/src/JavaOops/inventedPyramid.java
@@ -0,0 +1,21 @@
+package JavaOops;
+
+public class inventedPyramid {
+
+	public static void main(String[] args) {
+		
+		int k=1;
+		for(int i = 1;i<5;i++) {
+			
+			for(int j =1;j<=i;j++) {
+				System.out.print(k);
+				System.out.print("\t");
+				k++;
+				
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
