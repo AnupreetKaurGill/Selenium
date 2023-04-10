9c252203ab7e8b255920b83693afea06ee740b67
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-04-10 15:57:14 -0400

    multidimensional array

---
+++ Introduction/src/JavaOops/minNumberMultiDimArray.java
@@ -0,0 +1,47 @@
+package JavaOops;
+
+public class minNumberMultiDimArray {
+
+	/*2 4 5
+
+	3 4 7
+
+	1 2 9*/
+	//Maximum number from the array
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+		
+	int[][] abc = {{2,4,5 }, {3,4, 7} , {1,2,9}};
+	int min=abc[0][0];
+	int mincoloumn = 0;
+
+//print min number
+	for(int i=0;i<3;i++)
+	{
+	for(int j=0;j<3;j++)
+	{
+	if(abc[i][j]<min)//2
+	{
+	min=abc[i][j];
+	mincoloumn=j;
+	}
+	}
+	}
+	//print max number in min number row
+	int max=abc[0][mincoloumn];
+	int k = 0;
+	while(k<3)
+	{
+	if(abc[k][mincoloumn]>max)
+	{
+	max=abc[k][mincoloumn];
+	}
+	k++;
+	}
+
+	System.out.println(max);
+	}
+
+}
