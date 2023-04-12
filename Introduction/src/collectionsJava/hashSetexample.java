15f2210952af38c39960b6cd6ed8a51763243e5a
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-04-12 14:13:44 -0400

    HashSet

---
+++ Introduction/src/collectionsJava/hashSetexample.java
@@ -0,0 +1,37 @@
+package collectionsJava;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+
+public class hashSetexample {
+
+	public static void main(String[] args) {
+		//HashSet treeset, LinkedHashset implements Set interface 
+		//does not accept duplicate values  
+		// There is no guarantee elements stored in sequential order..Random order
+		
+		HashSet<String> hs= new HashSet<String>(); 
+		hs.add("USA"); 
+		hs.add("UK"); 
+		hs.add("INDIA");
+		hs.add("he"); 
+		hs.add("she"); 
+		hs.add("INDIA"); 
+		System.out.println(hs); 
+		System.out.println(hs.remove("UK")); 
+		System.out.println(hs.isEmpty()); 
+		System.out.println(hs.size()); 
+		
+		Iterator<String> i=hs.iterator(); 
+		
+		while(i.hasNext()) 
+		{ 
+			System.out.println(i.next());
+		}
+		}
+
+	}
+
+
