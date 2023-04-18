b979a97ef2841faa3160b6e792c331b682d10437
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-04-18 09:51:54 -0400

    Excel Data Driven

---
+++ ExcelDriven/src/test/java/testSample.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class testSample {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		dataDriven d = new dataDriven();
+		ArrayList data = d.getData("Add Profile");
+		
+		System.out.println(data.get(0));
+		System.out.println(data.get(1));
+		System.out.println(data.get(2));
+		System.out.println(data.get(3));
+
+	}
+
+	
+}
