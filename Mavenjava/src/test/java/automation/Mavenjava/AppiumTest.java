f2ec6e2415dd5aa40f35ff7b776e4271e21324af
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2023-04-21 13:38:27 -0400

    Maven

---
+++ Mavenjava/src/test/java/automation/Mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package automation.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAPpAndroid()
+	{
+		System.out.println("NativeAPpAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");	
+	}
+}
---
+++ Mavenjava/src/test/java/automation/Mavenjava/SeleniumTest.java
@@ -0,0 +1,19 @@
+package automation.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	//testng xml file from Maven
+	// how to execute all test cases from Test folder  - mvn test
+	@Test
+	public void BrowserAUtomation()
+	{
+		System.out.println("BrowserAUtomation");	
+	}
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");	
+	}
+	
+}
